3518a1f2cbc2da47b528cff6f9b0bf5d2135153c
ahmedfalah666 <ahmedfalahthekingofhack@gmail.com>
2026-07-12 20:46:22 +0300

    Remove test artifacts from repo

--- Bot_Code_DB/run_pc.ps1
+++
@@ -1,3 +0,0 @@
-$log = "C:\Users\Work - Study (Grind)\Desktop\Fast_Foreward_Bots\Bot_Code_DB\pc_bot.log"
-"=== BOT START at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') ===" | Out-File $log -Encoding utf8
-python -u main.py *>&1 | Out-File $log -Encoding utf8 -Append
